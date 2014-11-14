cf7b3a32cfed3e682432d602c10b9f2a37dc0d41
vladimir vasilyev <vladi_it@mail.ru>
2014-11-14 11:48:46 +0300

    add after axercices 9chapter

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -8,6 +8,8 @@ describe "Authentication" do
 
 		it { should have_content('Sign in') }
 		it { should have_title('Sign in') }
+		it { should_not have_link('Profile') }
+		it { should_not have_link('Settings') }
 	end
 
 	describe "signin" do
@@ -46,15 +48,28 @@ describe "Authentication" do
 			describe "when attempting to visit a protected page" do
 				before do
 					visit edit_user_path(user)
-					fill_in "Email",	with: user.email
-					fill_in "Password",	with: user.password
-					click_button "Sign in"
+					sign_in user, no_capybara: false
 				end
 
 				describe "after signing in" do
 
 					it "should render the desired protected page" do
 						expect(page).to have_title('Edit user')
+					end
+
+					describe "when signing in again" do
+						before do
+							click_link "Sign out"
+							#delete signout_path
+							visit signin_path
+							fill_in "Email", with: user.email
+							fill_in "Password", with: user.password
+							click_button "Sign in"
+						end
+
+						it "should render the default (profile) page" do
+							expect(page).to have_title(user.name)
+						end
 					end
 				end
 			end
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -90,14 +90,6 @@ describe "User pages" do
         before { click_button submit }
         let(:user) { User.find_by(email: 'user@example.com') }
 
-        it { should have_title(user.name) }
-        it { should have_selector('div.alert.alert-success', text: 'Welcome') }
-      end
-
-      describe "after saving the user" do
-        before { click_button submit }
-        let(:user) { User.find_by(email: 'user@example.com') }
-
         it { should have_link('Sign out') }
         it { should have_title(user.name) }
         it { should have_selector('div.alert.alert-success', text: 'Welcome') }
@@ -146,6 +138,18 @@ describe "User pages" do
       it { should have_link('Sign out', href: signout_path) }
       specify { expect(user.reload.name).to eq new_name }
       specify { expect(user.reload.email).to eq new_email }
+    end
+
+    describe "forbidden attributes" do
+      let(:params) do
+        { user: { admin: true, password: user.password,
+                  password_confirmation: user.password } }
+      end
+      before do
+        sign_in user, no_capybara: true
+        patch user_path(user), params
+      end
+      specify { expect(user.reload).not_to be_admin }
     end
   end
 end
